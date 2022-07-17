361baa646653bad55ba5da7c3e61ea799a413c7f
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2022-07-18 02:15:30 +0700

    fix Detail UI

--- src/components/banner/BannerItem.js
+++ src/components/banner/BannerItem.js
@@ -1,5 +1,6 @@
 export default function BannerItem({ item }) {
-  const { poster_path, title } = item;
+  const { poster_path, title, genres } = item;
+  console.log(item);
   return (
     <div className="w-full h-full relative rounded-lg">
       <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg "></div>
@@ -12,14 +13,15 @@ export default function BannerItem({ item }) {
         <h2 className="font-bold text-3xl mb-5">{title}</h2>
         <div className="flex items-center gap-x-3 mb-8">
           <span className=" py-1 px-3 rounded-md border border-white">
+            Avenger
+          </span>
+
+          {/* <span className=" py-1 px-3 rounded-md border border-white">
             Avengers
           </span>
           <span className=" py-1 px-3 rounded-md border border-white">
             Avengers
-          </span>
-          <span className=" py-1 px-3 rounded-md border border-white">
-            Avengers
-          </span>
+          </span> */}
         </div>
         <button className="py-3 px-6 rounded-lg font-medium bg-primary text-white">
           Watch Now
--- src/pages/MovieDetailPage.js
+++ src/pages/MovieDetailPage.js
@@ -4,16 +4,14 @@ import { useParams } from "react-router-dom";
 import { fetcher } from "../config";
 import axios from "axios";
 import useSWR from "swr";
-// https://api.themoviedb.org/3/movie/{movie_id}?api_key=1a763884400befdbd957d043e8e9e19c
+import { SwiperSlide, Swiper } from "swiper/react";
+import MovieCard from "../components/movie/MovieCard";
+
 const getData = async (movieId) => {
-  try {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/movie/${movieId}?api_key=1a763884400befdbd957d043e8e9e19c`
-    );
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=1a763884400befdbd957d043e8e9e19c`
+  );
+  return response.data;
 };
 const getCreditData = async (movieId) => {
   const response = await axios.get(
@@ -21,71 +19,174 @@ const getCreditData = async (movieId) => {
   );
   return response.data;
 };
+const getVideo = async (movieId) => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=1a763884400befdbd957d043e8e9e19c`
+  );
+  return response.data;
+};
+
+const getSimilar = async (movieId) => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=1a763884400befdbd957d043e8e9e19c`
+  );
+  return response.data;
+};
+
 function MovieDetailPage() {
   const { movieId } = useParams();
   const [data, setData] = useState({});
-
   useEffect(() => {
     getData(movieId).then((data) => setData(data));
   }, [movieId]);
-  // console.log(data);
-  // const { data } = useSWR(
-  //   `https://api.themoviedb.org/3/movie/${movieId}?api_key=1a763884400befdbd957d043e8e9e19c`,
-  //   fetcher
-  // );
-  // console.log(data);
-  const { backdrop_path, poster_path, title, genres, overview } = data;
+  console.log(data);
+  const {
+    backdrop_path,
+    poster_path,
+    title,
+    genres,
+    overview,
+    release_date,
+    vote_average,
+    runtime,
+  } = data;
   return (
     <div className="page-container">
       <div className="h-[500px] relative ">
-        <div className="absolute inset-0 bg-black bg-opacity-70"></div>
+        <div className="absolute inset-0 bg-black bg-opacity-90"></div>
         <div
           className="w-full h-full bg-cover bg-no-repeat"
           style={{
             backgroundImage: `url(https://image.tmdb.org/t/p/original/${backdrop_path})`,
           }}
         ></div>
+
+        <div className="absolute flex top-0 left-0 h-full w-full px-8 items-center">
+          <div className=" h-[400px] shrink-0">
+            <img
+              src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+              className="w-auto h-full pb-10 object-cover rounded-xl mx-auto"
+              alt=""
+            />
+          </div>
+          <div className="px-6">
+            <h1 className="text-4xl font-bold text-primary">
+              {title}
+              <span className="opacity-80 text-3xl px-2">
+                ({new Date(release_date).getFullYear()})
+              </span>
+            </h1>
+            <div className="mb-5">
+              <span className="text-sm">
+                <span className="opacity-80">Average: </span>
+                {vote_average}/10
+              </span>
+              <span className="text-sm pl-4">
+                <span className="opacity-80">Run time: </span>
+                {runtime}/10
+              </span>
+            </div>
+            {genres?.length > 0 && (
+              <div className="flex items-center  mb-5 gap-x-5 ">
+                {genres.map((item) => (
+                  <span
+                    className="py-1 px-4 border border-white rounded"
+                    key={item.id}
+                  >
+                    {item.name}
+                  </span>
+                ))}
+              </div>
+            )}
+            <h4 className="text-xl font-bold pb-2 text-primary">Overview</h4>
+            <p className=" text-sm leading-relaxed  mx-auto pb-10">
+              {overview}
+            </p>
+          </div>
+        </div>
       </div>
 
-      <div className="page-container h-[400px] -mt-[300px] relative">
-        <img
-          src={`https://image.tmdb.org/t/p/original/${poster_path}`}
-          className="w-auto h-full pb-10 object-cover rounded-xl mx-auto"
-          alt=""
-        />
-      </div>
-      <h1 className="text-center text-4xl font-bold text-white mb-10">
-        {title}
-      </h1>
-      {genres?.length > 0 && (
-        <div className="flex items-center justify-center mb-10 gap-x-5 ">
-          {genres.map((item) => (
-            <span
-              className="py-1 px-4 border border-primary text-primary rounded"
-              key={item.id}
-            >
-              {item.name}
-            </span>
-          ))}
-        </div>
-      )}
-      <p className="text-center text-sm leading-relaxed w-[80%] mx-auto pb-10">
-        {overview}
-      </p>
       <MovieCredit></MovieCredit>
+      <MovieVideo></MovieVideo>
+      <MovieSimilar></MovieSimilar>
     </div>
   );
 }
 
 function MovieCredit() {
   const { movieId } = useParams();
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   useEffect(() => {
     getCreditData(movieId).then((data) => setData(data));
   }, [movieId]);
   const { cast } = data;
-  return <>
-    <h2 className="text-center text-2xl pb-10">Series Cast</h2>
-  </>;
+  return (
+    <>
+      <h2 className="text-center text-3xl pb-10">Series Cast</h2>
+      <Swiper grapcursor="true" spaceBetween={40} slidesPerView={5}>
+        {cast?.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className="cast-item">
+              <img
+                src={`https://image.tmdb.org/t/p/original/${item.profile_path}`}
+                alt=""
+                className="w-full object-cover rounded-lg"
+              />
+            </div>
+            <h3 className="text-xl">{item.name}</h3>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
+
+function MovieVideo() {
+  const { movieId } = useParams();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getVideo(movieId).then((data) => setData(data));
+  }, [movieId]);
+
+  const { results } = data;
+  return (
+    <div className="py-10 w-full aspect-video">
+      {results?.slice(-1)?.map((item) => (
+        <iframe
+          id={item.id}
+          className="w-full h-full object-fill"
+          src={`https://www.youtube.com/embed/${item.key}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      ))}
+    </div>
+  );
+}
+
+function MovieSimilar() {
+  const { movieId } = useParams();
+  const [data, setData] = useState({});
+  useEffect(() => {
+    getSimilar(movieId).then((data) => setData(data));
+  }, [movieId]);
+  const { results } = data;
+
+  return (
+    <div className="py-10">
+      <h2 className="text-3xl mb-10">Similar movies</h2>
+      <div className="movie-list">
+        <Swiper grapcursor="true" spaceBetween={40} slidesPerView={3}>
+          {results?.map((item) => (
+            <SwiperSlide key={item.id}>
+              <MovieCard item={item}></MovieCard>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 }
 export default MovieDetailPage;
