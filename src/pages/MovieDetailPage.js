5bcb8c3488f570b017654a6e2c1a6c3cc848bcea
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2022-07-18 15:46:05 +0700

    fix URL useSWR

--- src/pages/MovieDetailPage.js
+++ src/pages/MovieDetailPage.js
@@ -2,44 +2,19 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 // import useSWR from "swr";
 import { fetcher } from "../config";
-import axios from "axios";
 import useSWR from "swr";
 import { SwiperSlide, Swiper } from "swiper/react";
 import MovieCard from "../components/movie/MovieCard";
 
-const getData = async (movieId) => {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=1a763884400befdbd957d043e8e9e19c`
-  );
-  return response.data;
-};
-const getCreditData = async (movieId) => {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=1a763884400befdbd957d043e8e9e19c`
-  );
-  return response.data;
-};
-const getVideo = async (movieId) => {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=1a763884400befdbd957d043e8e9e19c`
-  );
-  return response.data;
-};
-
-const getSimilar = async (movieId) => {
-  const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=1a763884400befdbd957d043e8e9e19c`
-  );
-  return response.data;
-};
-
 function MovieDetailPage() {
   const { movieId } = useParams();
-  const [data, setData] = useState({});
-  useEffect(() => {
-    getData(movieId).then((data) => setData(data));
-  }, [movieId]);
-  console.log(data);
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=1a763884400befdbd957d043e8e9e19c`,
+    fetcher
+  );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
   const {
     backdrop_path,
     poster_path,
@@ -51,8 +26,8 @@ function MovieDetailPage() {
     runtime,
   } = data;
   return (
-    <div className="page-container">
-      <div className="h-[500px] relative ">
+    <div className="page-container ">
+      <div className="h-[500px] relative mb-10">
         <div className="absolute inset-0 bg-black bg-opacity-90"></div>
         <div
           className="w-full h-full bg-cover bg-no-repeat"
@@ -83,11 +58,11 @@ function MovieDetailPage() {
               </span>
               <span className="text-sm pl-4">
                 <span className="opacity-80">Run time: </span>
-                {runtime}/10
+                {runtime}
               </span>
             </div>
             {genres?.length > 0 && (
-              <div className="flex items-center  mb-5 gap-x-5 ">
+              <div className="flex items-center flex-wrap mb-5 gap-x-5 gap-y-3 ">
                 {genres.map((item) => (
                   <span
                     className="py-1 px-4 border border-white rounded"
@@ -115,14 +90,18 @@ function MovieDetailPage() {
 
 function MovieCredit() {
   const { movieId } = useParams();
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    getCreditData(movieId).then((data) => setData(data));
-  }, [movieId]);
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=1a763884400befdbd957d043e8e9e19c`,
+    fetcher
+  );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
   const { cast } = data;
   return (
-    <>
-      <h2 className="text-center text-3xl pb-10">Series Cast</h2>
+    <div className="py-10">
+      <h2 className="capitalize text-primary mb-6 text-3xl font-bold">
+        Series Cast
+      </h2>
       <Swiper grapcursor="true" spaceBetween={40} slidesPerView={5}>
         {cast?.map((item) => (
           <SwiperSlide key={item.id}>
@@ -137,46 +116,61 @@ function MovieCredit() {
           </SwiperSlide>
         ))}
       </Swiper>
-    </>
+    </div>
   );
 }
 
 function MovieVideo() {
   const { movieId } = useParams();
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    getVideo(movieId).then((data) => setData(data));
-  }, [movieId]);
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=1a763884400befdbd957d043e8e9e19c`,
+    fetcher
+  );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
 
   const { results } = data;
   return (
-    <div className="py-10 w-full aspect-video">
-      {results?.slice(-1)?.map((item) => (
-        <iframe
-          id={item.id}
-          className="w-full h-full object-fill"
-          src={`https://www.youtube.com/embed/${item.key}`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      ))}
-    </div>
+    <>
+      {results
+        ?.filter((item) => {
+          return item.type === "Trailer" && item;
+        })
+        ?.map((item) => (
+          <div key={item.id} className="w-full aspect-video py-10">
+            <h2 className="capitalize text-primary mb-6 text-3xl font-bold">
+              {item.name}
+            </h2>
+            <iframe
+              id={item.id}
+              className="w-full h-full object-fill"
+              src={`https://www.youtube.com/embed/${item.key}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
+    </>
   );
 }
 
 function MovieSimilar() {
   const { movieId } = useParams();
-  const [data, setData] = useState({});
-  useEffect(() => {
-    getSimilar(movieId).then((data) => setData(data));
-  }, [movieId]);
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=1a763884400befdbd957d043e8e9e19c`,
+    fetcher
+  );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
   const { results } = data;
 
   return (
     <div className="py-10">
-      <h2 className="text-3xl mb-10">Similar movies</h2>
+      <h2 className="capitalize text-primary mb-6 text-3xl font-bold">
+        Similar Movies
+      </h2>
       <div className="movie-list">
         <Swiper grapcursor="true" spaceBetween={40} slidesPerView={3}>
           {results?.map((item) => (
