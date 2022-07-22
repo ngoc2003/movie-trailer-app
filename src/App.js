ab34da5cae69e7ba65fb21441902c1cc7069965a
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2022-07-22 13:30:15 +0700

    fix Scroll Top

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import CommingSoon from "./components/CommingSoon";
 import Loading from "./components/Loading";
 import "swiper/scss";
+import ScrollToTop from "./components/ScrollToTop";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
 const Main = lazy(() => import("./components/layout/Main"));
@@ -12,36 +13,41 @@ const MovieDetailPage = lazy(() => import("./pages/MovieDetailPage"));
 function App() {
   return (
     <Suspense fallback={<></>}>
-      <Loading>
-        <Routes>
-          <Route element={<Main></Main>}>
-            <Route path="/" element={<HomePage></HomePage>}></Route>
-            <Route exact path="/movie" element={<Movies></Movies>}></Route>
-            <Route path="/discover" element={<Movies></Movies>}></Route>
-            <Route path="/trending" element={<Movies></Movies>}></Route>
-            <Route path="/top_rated" element={<Movies></Movies>}></Route>
-            <Route path="/tv" element={<TvSeries></TvSeries>}></Route>
-            <Route
-              path="/tv/:movieId"
-              element={<MovieDetailPage></MovieDetailPage>}
-            ></Route>
-            <Route
-              path="/movie/:movieId"
-              element={<MovieDetailPage></MovieDetailPage>}
-            ></Route>
-            <Route
-              path="/community"
-              element={<CommingSoon></CommingSoon>}
-            ></Route>
-            <Route path="/recent" element={<CommingSoon></CommingSoon>}></Route>
-            {/* <Route
+      <ScrollToTop>
+        <Loading>
+          <Routes>
+            <Route element={<Main></Main>}>
+              <Route path="/" element={<HomePage></HomePage>}></Route>
+              <Route exact path="/movie" element={<Movies></Movies>}></Route>
+              <Route path="/discover" element={<Movies></Movies>}></Route>
+              <Route path="/trending" element={<Movies></Movies>}></Route>
+              <Route path="/top_rated" element={<Movies></Movies>}></Route>
+              <Route path="/tv" element={<TvSeries></TvSeries>}></Route>
+              <Route
+                path="/tv/:movieId"
+                element={<MovieDetailPage></MovieDetailPage>}
+              ></Route>
+              <Route
+                path="/movie/:movieId"
+                element={<MovieDetailPage></MovieDetailPage>}
+              ></Route>
+              <Route
+                path="/community"
+                element={<CommingSoon></CommingSoon>}
+              ></Route>
+              <Route
+                path="/recent"
+                element={<CommingSoon></CommingSoon>}
+              ></Route>
+              {/* <Route
               path="/setting"
               element={<CommingSoon></CommingSoon>}
             ></Route>
             <Route path="/log" element={<CommingSoon></CommingSoon>}></Route> */}
-          </Route>
-        </Routes>
-      </Loading>
+            </Route>
+          </Routes>
+        </Loading>
+      </ScrollToTop>
     </Suspense>
   );
 }
