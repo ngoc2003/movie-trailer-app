e0f86c0dfb6c861fbbfe78aa033c5708b2045bb1
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2022-07-22 13:54:31 +0700

    fix overflow banner

--- src/components/banner/BannerItem.js
+++ src/components/banner/BannerItem.js
@@ -13,13 +13,13 @@ export default function BannerItem({ item }) {
   const { genres } = data;
   return (
     <div className="w-full h-full relative rounded-lg overflow-hidden">
-      <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg "></div>
+      <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.6)] to-[rgba(0,0,0,0.6)] rounded-lg "></div>
       <img
         className="w-full h-full object-contain rounded-lg"
         src={API.getImageUrl(poster_path, "w500")}
         alt=""
       />
-      <div className="content absolute left-5 bottom-5 w-full text-white">
+      <div className="content absolute left-0 bottom-0 w-full p-3 text-white">
         <h2 className="capitalize text-white mb-6 font-bold text-4xl">{title}</h2>
         <div className="flex items-center flex-wrap mb-8">
           {genres?.map((genre) => (
